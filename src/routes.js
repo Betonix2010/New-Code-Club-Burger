987e0cc51407e0ab9150fbf9666bd830de43374f
Betonix2010 <betotb0@gmail.com>
2024-03-14 18:54:27 -0300

    Criando Carrosel de Categorias Backend

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,11 @@ import multerConfig from './config/multer'
 
 import ProductController from "./app/controllers/ProductController"
 import UserController from "./app/controllers/UserController"
-
+import CategoryController from "./app/controllers/CategoryController"
 import SessionController from "./app/controllers/SessionController"
+import OrderController from "./app/controllers/OrderController"
+
+import authMiddleware from './app/middlewares/auth'
 
 const upload = multer(multerConfig)
 
@@ -15,7 +18,18 @@ routes.post('/users', UserController.store)
 
 routes.post('/sessions', SessionController.store)
 
+routes.use(authMiddleware) // será chamado por todas as rotas ABAIXO 
+
 routes.post('/products', upload.single('file'), ProductController.store)
 routes.get('/products', ProductController.index)
+routes.put('/products/:id', upload.single('file'), ProductController.update)
+
+routes.post('/categories', upload.single('file'), CategoryController.store)
+routes.get('/categories', CategoryController.index)
+routes.put('/categories/:id', upload.single('file'), CategoryController.update)
+
+routes.post('/orders', OrderController.store)
+routes.put('/orders/:id', OrderController.update)
+routes.get('/orders', OrderController.index)
 
 export default routes 
